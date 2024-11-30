e76e7b7e21fec8aa36ce7eefd01f201676cefc28
nutc2705 <kai810425@gmail.com>
2024-11-30 18:21:40 +0800

    '1130'

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var { promisePool: mysql } = require('../lib/mysql');
+const argon2 = require('argon2');
 
 /* 顯示登入頁面 */
 router.get('/login', function(req, res, next) {
@@ -9,35 +11,48 @@ router.get('/login', function(req, res, next) {
 });
 
 /* 送出登入頁面 */
-router.post('/login', function(req, res, next) {
-  console.log({
-    body: req.body,
-    cookie: req.cookies,
-  });
+router.post('/login', async function(req, res, next) {
+  const [rows,fields] = await mysql.execute(
+    'SELECT id, password FROM users WHERE account = ?', 
+    [req.body.account]
+  );
 
-  const accounts = [
-    {
-      account: 'test',
-      password: '123456',
-    },
-    {
-      account: 'test2',
-      password: '123456',
-    },
-  ];
-
-  const login = accounts.find((account) => {
-    return account.account === req.body.account && account.password === req.body.password;
-  });
-
-  if (login) {
-    res.cookie('account', req.body.account, {signed: true});
-    res.redirect('/');
+  if (rows.length === 0) {
+    return res.redirect('/users/login?message=登入失敗');
   }
+
+  const login = await argon2.verify(rows[0].password, req.body.password);
 
   if (!login) {
-    res.redirect('/users/login?message=登入失敗');
+    return res.redirect('/users/login?message=登入失敗');
   }
+  
+  res.cookie('user_id', rows[0].id, {signed: true});
+  res.cookie('account', rows[0].account, {signed: true});
+  res.redirect('/');
+});
+
+/* 顯示註冊頁面 */
+router.get('/regist', function(req, res, next) {
+  const message = req.query?.message;
+
+  res.render('users/regist', { title: 'Express', message });
+});
+
+/* 送出註冊頁面 */
+router.post('/regist', async function(req, res, next) {
+  const hash_password = await argon2.hash(req.body.password);
+
+  const [rows,fields] = await mysql.execute(
+    'INSERT INTO users (account, password) VALUES (?,?)', 
+    [req.body.account, hash_password]
+  );
+
+  if (rows.affectedRows === 1) {
+    return res.redirect('/users/login?message=註冊成功');
+  }
+
+  return res.redirect('/users/regist?message=註冊失敗');
 });
 
 module.exports = router;
---
+++ test1130.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- 主機： 127.0.0.1:3306
+-- 產生時間： 2024-11-30 10:20:37
+-- 伺服器版本： 8.0.31
+-- PHP 版本： 8.0.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- 資料庫： `test1130`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `message`
+--
+
+DROP TABLE IF EXISTS `message`;
+CREATE TABLE IF NOT EXISTS `message` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  `message` varchar(1024) NOT NULL,
+  `created_at` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `account` varchar(20) NOT NULL,
+  `password` varchar(1024) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- 傾印資料表的資料 `users`
+--
+
+INSERT INTO `users` (`id`, `account`, `password`) VALUES
+(1, 'test', '123456'),
+(2, 'test123', '123456'),
+(3, 'test1', '$argon2id$v=19$m=65536,t=3,p=4$pYG5+6CjI0dskCEQaHSDPg$CC6Ji6Xz9yqE8IaE2iPTGsNB588KxVJkRVKge4qWDGE'),
+(4, 'test2', '123456'),
+(5, 'test3', '123456'),
+(6, 'test5', '$argon2id$v=19$m=65536,t=3,p=4$4HBQgPAPzmQCEBSHApC9PA$NoweC179/SLtxNZerIO96AAW1lqM8mfDnShK3fOChhI'),
+(7, 'test6', '$argon2id$v=19$m=65536,t=3,p=4$XT0ecJDkMRlN35FyvE7kQw$ZsHeLOCFPmKJyfOPAC6cQvY3QRMmYcAviUmi1xQPeDk');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
