9169c042ed0e09005d4edd5e6a51e4262411abd2
Jack <jack@Jackde-MacBook-Pro.local>
2024-11-30 14:40:26 +0800

    註冊功能、密碼雜湊功能

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const argon2 = require('argon2');
 
 /* 顯示登入頁面 */
 router.get('/login', function(req, res, next) {
@@ -9,35 +10,42 @@ router.get('/login', function(req, res, next) {
 });
 
 /* 送出登入頁面 */
-router.post('/login', function(req, res, next) {
-  console.log({
-    body: req.body,
-    cookie: req.cookies,
-  });
+router.post('/login', async function(req, res, next) {
+  const { username, password } = req.body;
 
-  const accounts = [
-    {
-      account: 'test',
-      password: '123456',
-    },
-    {
-      account: 'test2',
-      password: '123456',
-    },
-  ];
+  const [rows,fields] = await mysql.execute('SELECT * FROM `user` WHERE username = ?', [username]);
 
-  const login = accounts.find((account) => {
-    return account.account === req.body.account && account.password === req.body.password;
-  });
-
-  if (login) {
-    res.cookie('account', req.body.account, {signed: true});
-    res.redirect('/');
+  if (rows.length != 1) {
+    return res.redirect('/users/login?message=帳號或密碼錯誤');
   }
 
-  if (!login) {
-    res.redirect('/users/login?message=登入失敗');
+  const user = rows[0];
+
+  if (!await argon2.verify(user.password, password)) {
+    return res.redirect('/users/login?message=帳號或密碼錯誤');
   }
+
+  res.cookie('account', user.account, {signed: true});
+  res.cookie('user_id', user.id, {signed: true});
+  res.redirect('/');
 });
+
+router.get('/register', function(req, res, next) {
+  res.render('user/register', { title: '註冊' });
+});
+
+router.post('/register', async function(req, res, next) {
+  const { username, password, name } = req.body;
+  const hashedPassowrd = await argon2.hash(password);
+
+  const [rows,fields] = await mysql.execute('INSERT INTO `user` (username, password) VALUES (?, ?, ?)', [username, hashedPassowrd]);
+
+  if(rows !== 1) {
+    return res.redirect('/?message=註冊失敗');
+  }
+
+  res.redirect('/?message=註冊成功');
+});
+
 
 module.exports = router;
