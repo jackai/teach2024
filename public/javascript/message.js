64da273720cb1480967bc7abf25eae134f2de4fd
nutc2705 <kai810425@gmail.com>
2024-12-14 18:11:43 +0800

    get api

--- public/javascript/message.js
+++ public/javascript/message.js
@@ -1,3 +1,17 @@
-const create_message = () => {
-    alert('test123');
+const create_message = async () => {
+    const response = await fetch('/message/', {
+        method: 'POST',
+        body: JSON.stringify({ message: "example" }),
+    });
+
+    const json = await response.json();
+
+    console.log(json);
 };
+
+const get_message = async () => {
+    const response = await fetch('/message/new-message');
+    const json = await response.json();
+
+    console.log(json);
+}
--- routes/message.js
+++ routes/message.js
@@ -17,6 +17,21 @@ router.get('/', async function(req, res, next) {
   res.render('message/index', { title: 'Express', account, rows });
 });
 
+router.get('/new-message', async function(req, res, next) {
+  const [rows,fields] = await mysql.execute(
+    'SELECT users.account AS account, message, created_at ' +
+    'FROM message ' +
+    'INNER JOIN `users` ON `users`.`id` = message.user_id ' +
+    'ORDER BY message.id DESC LIMIT 10', 
+    []
+  );
+
+  return res.json({
+    status: true,
+    data: rows,
+  });
+})
+
 router.post('/', async function(req, res, next) {
     const user_id = req.signedCookies?.user_id;
     const message = req.body?.message;
